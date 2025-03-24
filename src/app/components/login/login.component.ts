7c0351607b6edcd012038c767e1024840f778078
Eden Marom <EdenMarom@gmail.com>
2025-03-24 20:35:23 +0200

    Login - start

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../interfaces/user.model';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private store = inject(Store<ReadonlyArray<User>>);
+  private router = inject(Router);
+  email: string = "";
+  password: string = "";
+  
+
+  // addTask() {
+  //   this.store.dispatch(TasksApiActions.createTask({ newTask: this.newTask }));
+  //   this.router.navigate(['/tasks']);
+  // }
+
+
+  
   loginHandler(){
     console.log("login");
+    console.log(this.email);
+    console.log(this.password)
   }
 }
